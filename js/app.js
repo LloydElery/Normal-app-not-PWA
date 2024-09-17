483fb6f34a3234398a7a480aa768a265914fcadc
LloydElery <dennisgooglar@gmail.com>
2024-09-17 13:44:20 +0200

    Added manifest

--- js/app.js
+++ js/app.js
@@ -1,44 +1,54 @@
-const container = document.querySelector(".container");
+/*
+ * container Tar ett element som har klassen container
+ */
+const container = document.querySelector('.container');
+/*
+ * Skapar en array för kaffe samt bilder på kaffe
+ */
 const coffees = [
   {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
+    name: 'Perspiciatis',
+    image: 'images/coffee1.jpg',
   },
   {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
+    name: 'Voluptatem',
+    image: 'images/coffee2.jpg',
   },
   {
-    name: "Explicabo",
-    image: "images/coffee3.jpg"
+    name: 'Explicabo',
+    image: 'images/coffee3.jpg',
   },
   {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
+    name: 'Rchitecto',
+    image: 'images/coffee4.jpg',
   },
   {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
+    name: ' Beatae',
+    image: 'images/coffee5.jpg',
   },
   {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
+    name: ' Vitae',
+    image: 'images/coffee6.jpg',
   },
   {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
+    name: 'Inventore',
+    image: 'images/coffee7.jpg',
   },
   {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
+    name: 'Veritatis',
+    image: 'images/coffee8.jpg',
   },
   {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
-  }
+    name: 'Accusantium',
+    image: 'images/coffee9.jpg',
+  },
 ];
+
+/* För varje kaffe vill vi plocka ut namn och bild
+ * samt skriva ut dessa i vår html
+ */
 const showCoffees = () => {
-  let output = "";
+  let output = '';
   coffees.forEach(
     ({ name, image }) =>
       (output += `
@@ -52,4 +62,4 @@ const showCoffees = () => {
   container.innerHTML = output;
 };
 
-document.addEventListener("DOMContentLoaded", showCoffees);
+document.addEventListener('DOMContentLoaded', showCoffees);
